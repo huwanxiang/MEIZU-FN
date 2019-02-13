72955dabfed16ab68ad97693f2ff28bacc00b646
WZH9958518 <1758076379@qq.com>
2019-02-13 16:34:25 +0800

    完成了导航显示js

--- js/index.js
+++ js/index.js
@@ -26,7 +26,6 @@ var swiper = new Swiper('.swiper-container', {
         }
     },
 });
-
 // 导航移入显示效果
 var showNav = (function () {
     return {
@@ -40,28 +39,43 @@ var showNav = (function () {
             return document.querySelectorAll(ele);
         },
         event() {
-            var _this = this;
+            var _this = this,
+                $allShow = this.$all('#header .nav_hover'),
+                $allShow_list = this.$all('#show-navList .type_area')
             this.$('#header').addEventListener('mouseover',
                 function (e) {
                     e = e || window.event;
                     var target = e.target || e.srcElement;
                     if (target.classList[1] == "nav_hover") {
-                        _this.SetStyle('block', 'white', {'height': 278}, 300, '#666666', target)
+                        _this.SetStyle('block', 'white', {'height': 198}, 300, '#666666', target)
                     }
                     else if (target.classList[0] == "getColor") {
-                        _this.SetStyle('none', 'transparent', {'height': 80}, 100, 'white', target)
+                        _this.SetStyle('none', 'transparent', {'height': 0}, 100, 'white', target)
                     }
-                },)
+                },false)
             this.$('#header').addEventListener('mouseleave', function () {
-                _this.SetStyle('none', 'transparent', {'height': 80}, 100, 'white')
-            })
-        },
+                _this.SetStyle('none', 'transparent', {'height': 0}, 100, 'white')
+            },false)
 
+            for (var a = 0;a<$allShow.length;a++){
+                $allShow[a].index = a;
+                $allShow[a].addEventListener('mouseenter',function (e) {
+                    for (var b = 0;b<$allShow.length;b++){
+                        $allShow_list[b].style.zIndex = 1;
+                        $allShow_list[b].style.opacity = 0;
+                    }
+                    e = e || window.event;
+                    var target = e.target||e.srcElement;
+                    $allShow_list[target.index].style.zIndex = 999;
+                    $allShow_list[target.index].style.opacity = 1;
+                },false)
+            }
+        },
         SetStyle(display, color, targetObj, time, color2, thisTarget) {
             var _this = this;
             this.$('.header_hover').style.display = display;
             this.$('#header').style.backgroundColor = color;
-            move('#header', targetObj, time);
+            move('#show-navList', targetObj, time);
             for (var i = 0; i < this.$all('.getColor').length; i++) {
                 _this.$all('.getColor')[i].style.color = color2
             }
@@ -73,53 +87,3 @@ var showNav = (function () {
 }())
 showNav.init();
 
-
-//
-// function $(ele) {
-//     return document.querySelector(ele);
-// }
-//
-// function $all(ele) {
-//     return document.querySelectorAll(ele);
-// }
-//
-// $('#header').addEventListener('mouseover',
-//     function (e) {
-//         e = e || window.event;
-//         var target = e.target || e.srcElement;
-//         if (target.classList[1] == "nav_hover") {
-//
-//             $('.header_hover').style.display = 'block';
-//             $('#header').style.backgroundColor = 'white';
-//             move('#header', {'height': 278}, 300);
-//             for (var i = 0; i < $all('.getColor').length; i++) {
-//                 $all('.getColor')[i].style.color = 'black'
-//             }
-//             target.style.color = '#00b9f2';
-//
-//
-//         } else if (target.classList[0] == "getColor") {
-//
-//             $('.header_hover').style.display = 'none';
-//             $('#header').style.backgroundColor = 'transparent';
-//             move('#header', {'height': 80}, 100)
-//             for (var i = 0; i < $all('.getColor').length; i++) {
-//                 $all('.getColor')[i].style.color = 'white';
-//             }
-//             target.style.color = '#00b9f2';
-//
-//
-//         }
-//     },)
-// $('#header').addEventListener('mouseleave', function () {
-//
-//
-//     $('.header_hover').style.display = 'none';
-//     $('#header').style.backgroundColor = 'transparent';
-//     move('#header', {'height': 80}, 100)
-//     for (var i = 0; i < $all('.getColor').length; i++) {
-//         $all('.getColor')[i].style.color = 'white'
-//     }
-//
-// })
-//
